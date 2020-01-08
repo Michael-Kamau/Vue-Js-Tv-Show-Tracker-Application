862cc5cb71d5a406dee15087a5d7323c8923d173
Michael-Kamau <kamau.karitu@gmail.com>
2020-01-09 00:06:19 +0300

    added the node server together with its dependencies

---
+++ server.js
@@ -0,0 +1,34 @@
+//server.js
+
+//importing the necessary modules to use in the website
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3480;
+
+
+
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//handles the get requests coming to the root url
+app.get('/', (req, res) => {
+
+});
+//handles the post requests sent to the server's shows url.
+app.post('/shows', (req, res) => {
+
+});
+
+
+
+app.listen(port, () => console.log(`Schedule site is listening on port: ${port}!`));
+
+
